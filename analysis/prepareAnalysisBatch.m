6dee1bf722132b0fda2c5b2eac39dab8b7d2c111
giulianoiurilli <giuliano.iurilli@gmail.com>
2015-10-20 12:18:14 -0400

    cc

--- analysis/prepareAnalysisBatch.m
+++ analysis/prepareAnalysisBatch.m
@@ -24,7 +24,7 @@ for idxExperiment = 1 : length(List)
     
       parameters
     %
-    %     valveOnsetTimestamp
+         valveOnsetTimestamp
     %
             try
                 breathOnsetTimestamp
