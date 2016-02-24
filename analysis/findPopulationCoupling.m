d7fc15a79d02495d68124a2d5cfa13399c94b081
giuliano.iurilli <giuliano.iurill@gmail.com>
2016-02-24 12:16:36 -0500

    cc

--- analysis/findPopulationCoupling.m
+++ analysis/findPopulationCoupling.m
@@ -1,35 +1,35 @@
 %%
 tic
 fileToSave = 'coa_15_2_2.mat';
+startingFolder = pwd;
 odorsRearranged = 1:15; 
 odors = length(odorsRearranged);
+
 %%
-startingFolder = pwd;
-for idxExp = 1: length(espe) 
+
+for idxExp = 1 : length(List)
+    cartella = List{idxExp};
+    cd(cartella)
+    load('unitsNowarp.mat', 'shankNowarp');
+    load('parameters.mat');
     idxCell = 0;
     for idxShank = 1:4
         for idxUnit = 1:length(esp(idxExp).shankNowarp(idxShank).cell)
-            if esp(idxExp).shankNowarp(idxShank).cell(idxUnit).good == 1 
+            if esp(idxExp).shankNowarp(idxShank).cell(idxUnit).good == 1
                 idxCell = idxCell + 1;
             end
         end
     end
-end
-
-
-
-%%
-sdf_response = nan * ones(idxCell, odors * (floor(pre*1000 + 3 * 1000) - floor(pre*1000 - 1000) + 1));
-sdf_response_diff = nan * ones(idxCell, odors * (floor(pre*1000 + 3 * 1000) - floor(pre*1000 - 1000) + 1));
-cellLog = nan * ones(idxCell, 3);
-for idxExp = 1: length(espe) 
+    sdf_response = nan * ones(idxCell, odors * (floor(pre*1000 + 3 * 1000) - floor(pre*1000 - 1000) + 1) * n_trials);
+    sdf_response_diff = nan * ones(idxCell, odors * (floor(pre*1000 + 3 * 1000) - floor(pre*1000 - 1000) + 1) * n_trials);
+    cellLog = nan * ones(idxCell, 3);
     idxCell = 0;
     for idxShank = 1:4
         for idxUnit = 1:length(esp(idxExp).shankNowarp(idxShank).cell)
             if esp(idxExp).shankNowarp(idxShank).cell(idxUnit).good == 1 
                 idxCell = idxCell + 1;
                 idxO = 0;
-                response = nan * ones(odors, floor(pre*1000 + 3 * 1000) - floor(pre*1000 - 1000) + 1);
+                response = nan * ones(odors, (floor(pre*1000 + 3 * 1000) - floor(pre*1000 - 1000) + 1) * n_trials);
                 for idxOdor = odorsRearranged
                     idxO = idxO + 1;
                     app = shankNowarp(idxShank).cell(idxUnit).odor(idxOdor).sdf_trialNoWarp(:, floor(pre*1000 - 1000) : floor(pre*1000 + 3 * 1000));
@@ -37,7 +37,7 @@ for idxExp = 1: length(espe)
                 end
                 sdf_response(idxCell,:) = reshape(response, 1, size(response,1) * size(response,2));
                 sdf_response_mean = mean(sdf_response(idxCell,:));
-                sdf_response_diff(idxCell,:) = sdf_response - sdf_response_mean;
+                sdf_response_diff(idxCell,:) = sdf_response(idxCell,:) - sdf_response_mean;
                 cellLog(idxCell,:) = [idxExp, idxShank, idxUnit];
             end
         end
@@ -57,5 +57,6 @@ for idxExp = 1: length(espe)
 end
 
 cd(startingFolder)
-clearvars -except espe
+clearvars -except esp
 save(fileToSave, 'esp', '-append')
+toc
